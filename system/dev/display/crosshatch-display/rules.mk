3afbdf9ea61b11cab8befd9da3a954cd6c90a604
Zhuowei Zhang <zhuoweizhang@yahoo.com>
2019-01-07 14:10:36 -0800

    [crosshatch] rename the dummy driver to crosshatch

--- system/dev/display/crosshatch-display/rules.mk
+++ system/dev/display/crosshatch-display/rules.mk
@@ -1,4 +1,4 @@
-# Copyright 2018 The Fuchsia Authors. All rights reserved.
+# Copyright 2019 The Fuchsia Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
@@ -9,7 +9,7 @@ MODULE := $(LOCAL_DIR)
 MODULE_TYPE := driver
 
 MODULE_SRCS += \
-    $(LOCAL_DIR)/dummy-display.cpp \
+    $(LOCAL_DIR)/crosshatch-display.cpp \
 
 MODULE_STATIC_LIBS := \
     system/ulib/ddk \

ecac62b4873b48ef65af8e067ef454c1f2193073
Zhuowei Zhang <zhuoweizhang@yahoo.com>
2019-01-21 15:05:17 -0800

    [crosshatch] fix ram size, add reserved regions from device tree
    Change-Id: Id8f7b878799e7692cbfc6800206fb6b7962d0f42

--- kernel/target/arm64/board/crosshatch/boot-shim-config.h
+++ kernel/target/arm64/board/crosshatch/boot-shim-config.h
@@ -18,12 +18,67 @@ static const zbi_mem_range_t mem_config[] = {
     {
         .type = ZBI_MEM_RANGE_RAM,
 	.paddr = 0x80000000,
-        .length = 0x100000000, // 4GB
+        .length = 0xfe0a0000, // 4GB
     },
     {
         .type = ZBI_MEM_RANGE_PERIPHERAL,
         .paddr = 0x00000000,
         .length = 0x80000000,
+    },
+    {
+        // hyp_region: hyp_region@85700000
+        .type = ZBI_MEM_RANGE_RESERVED,
+        .paddr = 0x85700000,
+        .length = 0x600000,
+    },
+    {
+        // xbl_region: xbl_region@85e00000
+        .type = ZBI_MEM_RANGE_RESERVED,
+        .paddr = 0x85e00000,
+        .length = 0x100000,
+    },
+    {
+        // removed_region: removed_region@85fc0000
+        .type = ZBI_MEM_RANGE_RESERVED,
+        .paddr = 0x85fc0000,
+        .length = 0x2f40000,
+    },
+    {
+        // qseecom_mem: qseecom_region@0x8ab00000
+        .type = ZBI_MEM_RANGE_RESERVED,
+        .paddr = 0x8ab00000,
+        .length = 0x1400000,
+    },
+    // omitted: pil/fw regions (there's no support for loading firmware yet)
+    {
+        // cont_splash_memory: cont_splash_region@9d400000
+        .type = ZBI_MEM_RANGE_RESERVED,
+        .paddr = 0x9d400000,
+        .length = 0x02400000,
+    },
+    {
+        // easel_mem: easel_mem@9f800000
+        .type = ZBI_MEM_RANGE_RESERVED,
+        .paddr = 0x9f800000,
+        .length = 0x2000000,
+    },
+    {
+        // debug_info: debug_info@0
+        .type = ZBI_MEM_RANGE_RESERVED,
+        .paddr = 0xa1800000,
+        .length = 0x00010000,
+    },
+    {
+        // ramoops_mem: ramoops_region@a1810000
+        .type = ZBI_MEM_RANGE_RESERVED,
+        .paddr = 0xa1810000,
+        .length = 0x200000,
+    },
+    {
+        // ramoops_meta_mem: ramoops_meta_region@a1c10000
+        .type = ZBI_MEM_RANGE_RESERVED,
+        .paddr = 0xa1c10000,
+        .length = 0x1000,
     },
 };
 
